64660ce3d441861ebd688bc4d14fa22548efc864
Jilles Soeters <jilles@cloudflare.com>
2025-10-22 11:27:22 -0500

    Add correct wrangler types

--- worker/index.ts
+++ worker/index.ts
@@ -2,11 +2,16 @@ export default {
   fetch(request) {
     const url = new URL(request.url);
 
+    if (url.pathname.startsWith('/sandbox/')) {
+      // handle sandbox
+    }
+
     if (url.pathname.startsWith("/api/")) {
       return Response.json({
         name: "Cloudflare",
       });
     }
-		return new Response(null, { status: 404 });
+
+    return new Response(null, { status: 404 });
   },
 } satisfies ExportedHandler<Env>;
